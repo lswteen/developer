7bb4040f1a15ba8695c70ad9182b3dbb1c9d8ded
leesoonwoo <sunu@wemakeprice.com>
2018-04-17 11:54:39 +0900

    fix : test sample source

--- src/main/java/com/sunu/project/developer/DeveloperApplication.java
+++ src/main/java/com/sunu/project/developer/DeveloperApplication.java
@@ -1,12 +1,25 @@
 package com.sunu.project.developer;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeveloperApplication {
 
+	@Data
+	public static class TestProject{
+		private String outputString;
+
+		public void test(){
+			setOutputString("Test hello springboot");
+			System.out.println("==================================>".concat(outputString));
+		}
+	}
+
 	public static void main(String[] args) {
+		TestProject testProject = new TestProject();
+		testProject.test();
 		SpringApplication.run(DeveloperApplication.class, args);
 	}
 }
