21f041e9a94f733b688a6463499cd1bb4131c3b2
leesoonwoo <sunu@wemakeprice.com>
2018-04-17 11:32:02 +0900

    Initial commit

---
+++ src/main/java/com/sunu/project/developer/DeveloperApplication.java
@@ -0,0 +1,12 @@
+package com.sunu.project.developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sunu/project/developer/DeveloperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sunu.project.developer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeveloperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
